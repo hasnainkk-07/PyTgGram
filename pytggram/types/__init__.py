94d9ef75a88d860d48aaae155465a7d9fb0e2df2
Hasnain khan <161922960+hasnainkk-07@users.noreply.github.com>
2025-09-26 11:14:12 +0545

    ㅤㅤ

--- pytggram/types/__init__.py
+++ pytggram/types/__init__.py
@@ -5,10 +5,14 @@ from .callback import CallbackQuery
 from .inline import InlineQuery, InlineQueryResult, InlineQueryResultArticle, InputTextMessageContent
 from .poll import Poll, PollOption, PollAnswer
 from .payments import PreCheckoutQuery, ShippingQuery
+from .keyboard import (KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, 
+                      InlineKeyboardMarkup, ReplyKeyboardRemove, ForceReply)
 
 __all__ = [
     'Message', 'User', 'Chat', 'CallbackQuery', 
     'InlineQuery', 'InlineQueryResult', 'InlineQueryResultArticle', 'InputTextMessageContent',
     'Poll', 'PollOption', 'PollAnswer',
-    'PreCheckoutQuery', 'ShippingQuery'
+    'PreCheckoutQuery', 'ShippingQuery',
+    'KeyboardButton', 'ReplyKeyboardMarkup', 'InlineKeyboardButton', 
+    'InlineKeyboardMarkup', 'ReplyKeyboardRemove', 'ForceReply'
 ]
